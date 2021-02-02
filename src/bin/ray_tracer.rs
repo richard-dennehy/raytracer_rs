b6892ec216084077dead5eb7b23c0e524f08d376
Richard Dennehy <richard.dennehy@outlook.com>
2021-02-02 19:01:49 +0000

    example rendering wavefront file

--- src/bin/ray_tracer.rs
+++ src/bin/ray_tracer.rs
@@ -4,14 +4,16 @@ extern crate ray_tracer;
 extern crate nonzero_ext;
 
 use ray_tracer::*;
+use std::convert::TryInto;
 use std::f64::consts::PI;
+use std::fs;
 use std::num::NonZeroU16;
 use std::time::Instant;
 
-const CAMERA_WIDTH: NonZeroU16 = nonzero!(1920u16);
-const CAMERA_HEIGHT: NonZeroU16 = nonzero!(1080u16);
+const CAMERA_WIDTH: NonZeroU16 = nonzero!(400u16);
+const CAMERA_HEIGHT: NonZeroU16 = nonzero!(300u16);
 
-fn main() {
+fn main() -> Result<(), String> {
     let timer = Instant::now();
 
     let mut world = World::empty();
@@ -20,42 +22,22 @@ fn main() {
         Point3D::new(-10.0, 12.0, -10.0),
     ));
 
-    fn hexagon_corner() -> Object {
-        Object::sphere()
-            .with_transform(Matrix4D::uniform_scaling(0.25).with_translation(0.0, 0.0, -1.0))
-    }
+    let wavefront_file = fs::read_to_string("susan.obj").map_err(|e| e.to_string())?;
+    let susan: Object = obj_parser::parse(&wavefront_file).try_into()?;
+    let susan = susan.with_transform(
+        Matrix4D::rotation_x(PI / 2.0)
+            .with_rotation_z(PI)
+            .with_rotation_x(-PI / 2.0),
+    );
 
-    fn hexagon_edge() -> Object {
-        Object::cylinder()
-            .min_y(0.0)
-            .max_y(1.0)
-            .build()
-            .with_transform(
-                Matrix4D::scaling(0.25, 1.0, 0.25)
-                    .with_rotation_z(-PI / 2.0)
-                    .with_rotation_y(-PI / 6.0)
-                    .with_translation(0.0, 0.0, -1.0),
-            )
-    }
-
-    fn hexagon_side() -> Object {
-        Object::group(vec![hexagon_corner(), hexagon_edge()])
-    }
-
-    let hexagon_parts = (0..6)
-        .into_iter()
-        .map(|i| hexagon_side().with_transform(Matrix4D::rotation_y(i as f64 * PI / 3.0)))
-        .collect();
-    let hexagon = Object::group(hexagon_parts);
-
-    world.objects.push(hexagon);
+    world.objects.push(susan);
 
     let camera = Camera::new(
         CAMERA_WIDTH,
         CAMERA_HEIGHT,
         PI / 3.0,
         Matrix4D::view_transform(
-            Point3D::new(0.0, 2.5, -6.0),
+            Point3D::new(0.0, 0.0, -5.0),
             Point3D::new(0.0, 0.0, 0.0),
             Vector3D::new(0.0, 1.0, 0.0),
         ),
@@ -67,5 +49,7 @@ fn main() {
     let image = image_writer::write(canvas);
     image.save("out.png").expect("failed to write output file");
 
-    println!("Completed in {:.2?}", timer.elapsed())
+    println!("Completed in {:.2?}", timer.elapsed());
+
+    Ok(())
 }
